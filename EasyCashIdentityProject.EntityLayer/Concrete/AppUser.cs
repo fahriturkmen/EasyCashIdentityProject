1c6500ea09043fbe229bbbaa714644170f9600ee
fahriturkmen <fturkmen922@gmail.com>
2023-05-05 14:08:21 +0300

    Ders 5 Sonu

---
+++ EasyCashIdentityProject.EntityLayer/Concrete/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+	public class AppUser : IdentityUser<int>
+	{
+        public String? Name { get; set; }
+        public String? Surname { get; set; }
+        public String? District { get; set; }
+        public String? City { get; set; }
+        public String? ImageUrl { get; set; }
+    }
+}
