f2d66b77ca53ef329ac698fe9576d84304c265f5
fahriturkmen <fturkmen922@gmail.com>
2023-06-19 15:21:06 +0300

    27. Ders Sonu

---
+++ EasyCashIdentityProject.DataAccessLayer/Migrations/20230619110221_mig_CustomerAccountProcess_Add_Description.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyCashIdentityProject.DataAccessLayer.Migrations
+{
+    public partial class mig_CustomerAccountProcess_Add_Description : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProcessType",
+                table: "CustomerAccountProcesses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "CustomerAccountProcesses",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "CustomerAccountProcesses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProcessType",
+                table: "CustomerAccountProcesses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
